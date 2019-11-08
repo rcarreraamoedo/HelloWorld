8191d32fb0f0f9786189a2f6044304a2842e8739
rcarreraamoedo <rcarreraamoedo@danielcastelao.org>
2019-11-08 14:20:45 +0100

    Salida Saludo

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,8 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	System.out.println("Hello World!");
+	System.out.println("Bye");
+	//TODO
+        //Comprobar que el numero sea negativo
     }
 }
